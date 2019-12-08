579037224edc96739754d8edb9c220af409debe7
Ari Drookman <aga.drookman@gmail.com>
2019-12-08 11:23:13 -0500

    Created Deck

---
+++ war.rb
@@ -0,0 +1,11 @@
+### Creating a deck of 52 cards.
+jack = 11
+queen = 12
+king = 13
+ace = 14
+deck_cards = {
+	"Clubs" => [2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace],
+	"Diamonds" => [2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace],
+	"Hearts" => [2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace],
+	"Spades" => [2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace]
+}
