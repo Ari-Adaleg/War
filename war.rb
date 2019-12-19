788104b38cf3c763baf4a7b6ffd73e3c1278d3b8
Ari Drookman <aga.drookman@gmail.com>
2019-12-19 15:36:49 -0500

    War v. 1.0

--- war.rb
+++ war.rb
@@ -1,6 +1,3 @@
-### Creating a deck of 52 cards.
-
-
 jack = 11
 queen = 12
 king = 13
@@ -17,13 +14,8 @@ suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
 while !deck_cards.empty?
 	rand_suit1 = suits[rand(suits.length)]
-	rand_suit2 = suits[rand(suits.length)]
-
 	suit1 = deck_cards[rand_suit1]
-	suit2 = deck_cards[rand_suit2]
-
 	player1card = suit1[rand(suit1.length)]
-	player2card = suit2[rand(suit2.length)]
 
 	puts "Player 1 flipped the #{player1card} of #{rand_suit1}."
 	suit1.delete(player1card)
@@ -31,7 +23,11 @@ while !deck_cards.empty?
 		deck_cards.delete(rand_suit1)
 		suits.delete(rand_suit1)
 	end
-	
+
+	rand_suit2 = suits[rand(suits.length)]
+	suit2 = deck_cards[rand_suit2]
+	player2card = suit2[rand(suit2.length)]
+
 	puts "Player 2 flipped the #{player2card} of #{rand_suit2}."
 	suit2.delete(player2card)
 
@@ -39,6 +35,17 @@ while !deck_cards.empty?
 		deck_cards.delete(rand_suit2)
 		suits.delete(rand_suit2)
 	end
+
+	if player1card > player2card
+		puts "Player 1 won this round."
+	elsif player1card < player2card
+		puts "Player 2 won this round."
+	elsif player1card == player2card
+		puts "DRAW"
+	end
+
+	puts "Press ENTER to continue:"
+	gets
 end
 
 puts deck_cards
